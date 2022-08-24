cc928dfbc3a03d451ec058838c12bef14a176f13
bruhleal <bruna11leal@live.com>
2022-08-24 18:19:02 -0400

    requisitos 5,6,7 feitos

--- src/challenges.js
+++ src/challenges.js
@@ -26,19 +26,44 @@ function concatName(meuArray) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return wins * 3 + ties * 1;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(numbers) {
   // seu código aqui
+  let maior = numbers[0];
+  let count = 0;
+  for (let i = 0; i <= numbers.length; i += 1) {
+    if (numbers[i] > maior) {
+      maior = numbers[i];
+    }
+  }
+  for(let j=0; j <= numbers.length; j+=1 ){
+    if(numbers[j] === maior){
+      count+=1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let positionCat1 = Math.abs(cat1 - mouse);
+  let positionCat2 = Math.abs(cat2 - mouse);
+  if(positionCat1 < positionCat2){
+    return 'cat1';
+  } else if (positionCat2 <  positionCat1) {
+    return 'cat2';
+  } else if (positionCat1 === positionCat2){
+    return 'os gatos trombam e o rato foge';
+  }
 }
+
+
 
 // Desafio 8
 function fizzBuzz() {
